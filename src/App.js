06593e77a2fcf3e3d30d2efd032a2bc2ec4a89f6
Dipti Narwade <diptinarwade1@gmail.com>
2021-03-30 00:45:25 +0530

    implemented HOC concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Hero from "./HeroComponent/Hero";
+import StocksList from "./WrappedComponents/StocksList";
+import UserList from "./WrappedComponents/UserList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StocksData = [
+  {
+    id: 1,
+    name: "TCS",
+  },
+  {
+    id: 2,
+    name: "Infosys",
+  },
+  {
+    id: 3,
+    name: "Reliance",
+  },
+];
+const UsersData = [
+  {
+    id: 1,
+    name: "Krunal",
+  },
+  {
+    id: 2,
+    name: "Ankit",
+  },
+  {
+    id: 3,
+    name: "Rushabh",
+  },
+];
+const Users = Hero(UserList, UsersData);
+const Stocks=Hero(StocksList, StocksData);
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Users></Users>
+        <Stocks></Stocks>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/WrappedComponents/StocksList.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import TableRow from "./TableRow";
+
+class StocksList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  loadTableRow() {
+    if (this.props.data instanceof Array) {
+      return this.props.data.map((obj, i) => {
+        return <TableRow obj={obj} key={i} />;
+      });
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Name</td>
+            </tr>
+          </thead>
+          <tbody>{this.loadTableRow()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+export default StocksList;
